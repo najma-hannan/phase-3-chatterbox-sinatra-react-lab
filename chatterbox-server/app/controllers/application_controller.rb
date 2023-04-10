faae481e0d629143acce1ff596b18a5f36ca9e17
Hannan <najma.mahat@student.moringaschool.com>
2023-04-10 15:26:59 +0300

    Created the routes for the CRUD operations

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,32 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add routes
-  
+  get "/messages" do
+    messages = Message.all
+    messages.to_json
+  end
+
+  get "/messages/:id" do
+    message = Message.find(params[:id])
+    message.to_json
+  end
+
+  post "/messages" do
+    new_message =
+      Message.create(body: params[:body], username: params[:username])
+    new_message.to_json
+  end
+
+  patch "/messages/:id" do
+    updated_message = Message.find(params[:id])
+    updated_message.update(body: params[:body])
+    updated_message.to_json
+  end
+
+  delete "/messages/:id" do
+    deleted_message = Message.find(params[:id])
+    deleted_message.destroy
+    deleted_message.to_json
+  end
 end
