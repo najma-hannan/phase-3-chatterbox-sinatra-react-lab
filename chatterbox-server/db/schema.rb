984ed038f62a9d2444b84c415281be7443293aac
Hannan <najma.mahat@student.moringaschool.com>
2023-04-10 15:35:55 +0300

    Created the message model

--- chatterbox-server/db/schema.rb
+++ chatterbox-server/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_04_10_123354) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
